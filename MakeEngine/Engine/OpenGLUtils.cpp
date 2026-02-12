baa63573d72a18746d90b69bb491a0e5a9116073
make100games <make100games@gmail.com>
2026-02-12 19:55:46 +0100

    Add open GL util

---
+++ MakeEngine/Engine/OpenGLUtils.cpp
@@ -0,0 +1,15 @@
+//
+//  OpenGLUtils.cpp
+//  MakeEngine
+//
+//  Created by Make100Games on 12.02.26.
+//
+
+#include "OpenGLUtils.hpp"
+
+void checkGLError(const char* operation) {
+    GLenum err;
+    while ((err = glGetError()) != GL_NO_ERROR) {
+        std::cerr << "OpenGL error after " << operation << std::endl;
+    }
+}
---
+++ MakeEngine/Engine/OpenGLUtils.hpp
@@ -0,0 +1,19 @@
+//
+//  OpenGLUtils.hpp
+//  MakeEngine
+//
+//  Created by Make100Games on 12.02.26.
+//
+
+#ifndef OpenGLUtils_hpp
+#define OpenGLUtils_hpp
+
+#include <stdio.h>
+#include <OpenGL/gl3.h>
+#include <iostream>
+
+void checkGLError(const char* operation);
+
+
+
+#endif /* OpenGLUtils_hpp */
